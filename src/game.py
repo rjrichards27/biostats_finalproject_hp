496db652913832984e2d3aaae24a8b9b29d9f642
Rachel Richards <rjr49@duke.edu>
2023-04-18 15:09:40 -0400

    pushing code with flask app included

--- src/game.py
+++ src/game.py
@@ -10,7 +10,6 @@ class Game:
     # intializing objects for class
     characters_playing: list[dict[str, str]]
     spells: list[dict[str, str]]
-    champion: str
     total_rounds: int
 
     def bracket_maker(self) -> list[tuple[dict[str, str], dict[str, str]]]:
@@ -22,23 +21,20 @@ class Game:
 
     def print_duel_pairs(
         self, character_pairs_list: list[tuple[dict[str, str], dict[str, str]]]
-    ) -> None:
+    ) -> list[str]:
         """Print duel games before being played."""
-        print("Behold of all the duels to come!\n")
+        duels_list = []
         for duel_pairs in character_pairs_list:
-            print(
-                "Duel: ",
-                duel_pairs[0]["Name"],
-                "vs.",
-                duel_pairs[1]["Name"],
-            )
-            print("   ")
+            duel_title = f"{duel_pairs[0]['Name']} vs. {duel_pairs[1]['Name']}"
+            duels_list.append(duel_title)
+
+        return duels_list
 
     def duel(
         self,
         character_group: tuple[dict[str, str], dict[str, str]],
         spells: list[dict[str, str]],
-    ) -> int:
+    ) -> tuple[int, list[str]]:
         """Simulate a duel between two characters.
 
         The character group is the pair (or multiple) of characters)
@@ -57,6 +53,7 @@ class Game:
         battle_stats = dict()
         spells_used = dict()
         group_list = list(character_group)
+        duel_prints = []
 
         for i, character in enumerate(group_list):
             # calculating character stats
@@ -88,88 +85,100 @@ class Game:
             battle_stats[i] = character_stats + spell_stats
             spells_used[i] = chosen_spell
 
-            print(
-                f"{character['Name']} used {chosen_spell['Name']} "
-                + f"({chosen_spell['Description']})!"
+            duel_prints.append(
+                (
+                    f"{character['Name']} used {chosen_spell['Name']} "
+                    + f"({chosen_spell['Description']})!"
+                )
             )
-            print(
-                f"{character['Name']} has a total power"
-                f" rating of {battle_stats[i]}!\n"
+            duel_prints.append(
+                (
+                    f"{character['Name']} has a total power"
+                    f" rating of {battle_stats[i]}!"
+                )
             )
 
         # determining the winner
         # added scenario when the output of battle is a tie
         if battle_stats[0] > battle_stats[1]:
             winner_key = 0
-            print(
-                f"{character_group[winner_key]['Name']} defeated "
-                + f"{character_group[abs(winner_key-1)]['Name']}!\n\n"
+            duel_prints.append(
+                (
+                    f"{character_group[winner_key]['Name']} defeated "
+                    + f"{character_group[abs(winner_key-1)]['Name']}!"
+                )
             )
         elif battle_stats[0] == battle_stats[1]:
             winner_key = 0
-            print(
-                f"{character_group[winner_key]['Name']} cast the spell "
-                + f"first and defeated "
-                + f"{character_group[abs(winner_key-1)]['Name']}!\n"
+            duel_prints.append(
+                (
+                    f"{character_group[winner_key]['Name']} cast the spell "
+                    + f"first and defeated "
+                    + f"{character_group[abs(winner_key-1)]['Name']}!"
+                )
             )
         else:
             winner_key = 1
-            print(
-                f"{character_group[winner_key]['Name']} defeated "
-                + f"{character_group[abs(winner_key-1)]['Name']}!\n"
+            duel_prints.append(
+                (
+                    f"{character_group[winner_key]['Name']} defeated "
+                    + f"{character_group[abs(winner_key-1)]['Name']}!"
+                )
             )
 
         # To make the bracket update process simpler
         # the objective is to return the key (index)
         # of the winning character
-        return winner_key
+        return winner_key, duel_prints
 
-    def print_winner(self, winner: dict[str, str]) -> None:
+    def print_winner(self, winner: dict[str, str]) -> str:
         """Print the winner of the game."""
-        print(
-            f"The winner of the tournament is {winner['Name']}!\n"
-            + f"{winner['Bio']}\n"
+        return (
+            f"The winner of the tournament is {winner['Name']}! " + f"{winner['Bio']}"
         )
 
-    def play_game(self) -> None:
+    def play_game(self) -> tuple[list[str], str]:
         """Play the game."""
         # creating the bracket
         character_pairs_list = self.bracket_maker()
         # playing the bracket
         round = 1
         temp_winners = []
+        print_statements = []
         while round <= self.total_rounds:
-            print(f"Prepare your wands! May the best Wizard/Witch win!\n")
+            print_statements.append(
+                "Prepare your wands! May the best Wizard/Witch win!"
+            )
             # print("==================================================")
             for i, duel_pairs in enumerate(character_pairs_list):
                 if round == self.total_rounds:
                     pass
                 else:
-                    print(f"Round {round}, Duel {i+1}")
-                    print(
-                        "==================================================\n"
+                    print_statements.append(f"Round {round}, Duel {i+1}")
+                    print_statements.append(
+                        "=================================================="
                     )
                 winner_key = self.duel(duel_pairs, self.spells)
-                if (
-                    i + 1 == len(character_pairs_list)
-                    and round != self.total_rounds
-                ):
-                    print(
-                        "==================================================\n"
+                winner_index = winner_key[0]
+                print_statements.append(winner_key[-1])
+                if i + 1 == len(character_pairs_list) and round != self.total_rounds:
+                    print_statements.append(
+                        "=================================================="
                     )
-                    print(f"Round {round} is over!\n")
+                    print_statements.append(f"Round {round} is over!")
                 else:
                     pass
-                temp_winners.append(duel_pairs[winner_key])
+                temp_winners.append(duel_pairs[winner_index])
             if len(temp_winners) == 1:
-                self.print_winner(temp_winners[0])
-                if temp_winners[0]["Name"] == self.champion:
-                    print(f"Your champion is victorious!")
+                winner_name = temp_winners[0]["Name"]
+                print_statements.append(self.print_winner(temp_winners[0]))
+                """if temp_winners[0]["Name"] == self.champion:
+                    print_statements.append(f"Your champion is victorious!")
                 else:
-                    print(
+                    print_statements.append(
                         f"Your champion {self.champion} did not make it "
-                        f"to the end...\n"
-                    )
+                        f"to the end..."
+                    )"""
                 break
             else:
                 self.characters_playing = temp_winners
@@ -177,30 +186,28 @@ class Game:
                 temp_winners = []
                 round += 1
                 if len(character_pairs_list) == 1:
-                    print("==================================================")
-                    print("Final Duel!\n")
-                    print(
-                        "Duel: ",
-                        duel_pairs[0]["Name"],
-                        "vs.",
-                        duel_pairs[1]["Name"],
+                    print_statements.append(
+                        "=================================================="
                     )
-                    print("   ")
+                    print_statements.append("Final Duel!")
+                    print_statements.append(
+                        f"{duel_pairs[0]['Name']} vs. {duel_pairs[1]['Name']}"
+                    )
+                    print_statements.append("   ")
                 else:
-                    print("==================================================")
-                    print(
-                        f"Break is over!"
-                        f"While our survivors enjoy their butterbeer in the "
-                        f"break room, let's see the next set of duels:\n"
+                    print_statements.append(
+                        "=================================================="
+                    )
+                    print_statements.append(
+                        f"Break is over! "
+                        f" While our survivors enjoy their butterbeer in the "
+                        f"break room, let's see the next set of duels: "
                     )
                     for duel_pairs in character_pairs_list:
-                        print(
-                            "Duel:",
-                            duel_pairs[0]["Name"],
-                            "vs.",
-                            duel_pairs[1]["Name"],
-                            "\n",
+                        print_statements.append(
+                            f"{duel_pairs[0]['Name']} vs. {duel_pairs[1]['Name']}"
                         )
+        return print_statements, winner_name
 
 
 # intitializing character list
@@ -246,15 +253,11 @@ def give_character_choice(characters: list[dict[str, str]]) -> dict[str, str]:
 
 
 # reading in data for characters and spells
-hp_characters = (
-    "../data/Harry_Potter_Characters.tsv"
-)
+hp_characters = "../data/Harry_Potter_Characters.tsv"
 characters_dict = read_data(hp_characters)
 
 # reading in data for characters and spells
-hp_spells = (
-    "../data/Harry_Potter_Spells.tsv"
-)
+hp_spells = "../data/Harry_Potter_Spells.tsv"
 spells_dict = read_data(hp_spells)
 
 
@@ -273,11 +276,11 @@ if __name__ == "__main__":
             f"to play? (2-5) "
         )
     )
-    selected_chars = random.sample(
-        characters_dict, rounds_character_num[total_rounds]
-    )
+    selected_chars = random.sample(characters_dict, rounds_character_num[total_rounds])
     champion = give_character_choice(selected_chars)
-    game1 = Game(selected_chars, spells_dict, champion["Name"], total_rounds)
+    game1 = Game(selected_chars, spells_dict, total_rounds)
     bracket = game1.bracket_maker()
-    game1.print_duel_pairs(bracket)
-    game1.play_game()
+    duels = game1.print_duel_pairs(bracket)
+    print(duels)
+    prints = game1.play_game()
+    print(prints[0])
